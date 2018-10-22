2bfc2c00c6d707f834f414053edbe38dd80d6c5e
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-10-22 15:15:39 -0300

    Remove of babel-polyfill dependency (embeded). Added ui5-babel-polyfills dependency (UI5 Library in the server).

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,6 @@
 import UIComponent from "sap/ui/core/UIComponent";
 import models from "sap/ui/demo/basicTemplate/model/models";
 
-/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "polyfill" }]*/
-import polyfill from "sap/ui/demo/basicTemplate/lib/polyfill";
-
 export default UIComponent.extend("sap.ui.demo.basicTemplate.Component", {
 
     metadata: {
