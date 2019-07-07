450f47e1f18fcae044d3ef7b3074cf3582a1dea2
Juan Sebastián Goldberg <sebas.goldberg@gmail.com>
2019-07-07 12:13:48 -0300

    ui5-babel-polyfills added as dependency to enable polyfills to enable ES6+ syntax in the browser.

--- src/Component.js
+++ src/Component.js
@@ -11,14 +11,23 @@ sap.ui.define([
 			manifest: "json"
 		},
 
+		testPolyfills: function () {
+			[].map(x=>x);
+			return new Promise( resolve => setTimeout(() => {
+				resolve();
+			}, 1));
+		},
+
 		/**
 		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
 		 * @public
 		 * @override
 		 */
-		init: function() {
+		init: async function() {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
+
+			await this.testPolyfills();
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
